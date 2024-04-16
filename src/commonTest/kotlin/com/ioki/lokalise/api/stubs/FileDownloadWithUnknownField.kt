2998e3ee8fe08d2e5bb2a449e844101216eb69d4
Fabian Keunecke <f@biankeunecke.de>
2024-04-16 19:06:54 +0200

    Ignore unknown response fields

---
+++ src/commonTest/kotlin/com/ioki/lokalise/api/stubs/FileDownloadWithUnknownField.kt
@@ -0,0 +1,9 @@
+package com.ioki.lokalise.api.stubs
+
+val fileDownloadWithUnknownFieldJson = """
+{
+  "project_id": "3002780358964f9bab5a92.87762498",
+  "bundle_url": "https://s3-eu-west-1.amazonaws.com/lokalise-assets/export/MyApp-locale.zip",
+  "unknown_field": 42
+}
+""".trimIndent()
