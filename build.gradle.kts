38d1fe5fb1d4f1b9f7e52398b2dfe888af52678f
Stefan M. <StefMa@users.noreply.github.com>
2024-04-19 19:06:03 +0000

    Prepare next release

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,7 +60,7 @@ val dokkaJar = tasks.register<Jar>("dokkaJar") {
 }
 
 group = "com.ioki"
-version = "0.0.3-SNAPSHOT"
+version = "0.0.3"
 publishing {
     publications {
         publications.withType<MavenPublication> {
