061c3b508c9101a269c26c80dfb00382315fb363
StefMa <StefMaDev@outlook.com>
2025-01-07 14:58:24 +0100

    Migrate to dokka v2

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,8 +54,8 @@ kotlin {
 }
 
 val dokkaJar = tasks.register<Jar>("dokkaJar") {
-    dependsOn(tasks.dokkaHtml)
-    from(tasks.dokkaHtml.flatMap { it.outputDirectory })
+    dependsOn(tasks.dokkaGenerate)
+    from(tasks.dokkaGeneratePublicationHtml.flatMap { it.outputDirectory })
     archiveClassifier.set("javadoc")
 }
 
